0826742333b62e3afd8c2f4c0552164b6791bc79
derushio <narumi18wa@gmail.com>
2019-03-29 17:07:04 +0900

    <master>[new file] fix predict

--- test/predict.py
+++ test/predict.py
@@ -18,11 +18,11 @@ with open('image-net-labels.json') as f:
     labels = json.load(f)
 
 ## データ読み込み
-# (x_train, y_train), (x_test, y_test) = cifar100.load_data()
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+# (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 ## データサンプリング
-x_test = x_test[:500]
+x_test = x_test[500:1000]
 
 ## 推論
 results = []
@@ -34,7 +34,7 @@ for i, array in enumerate(x_test):
     ## tensor化
     y = np.asarray(image)
     ## 次元拡張 (input tensorのshapeに合わせる)
-    if (len(a.shape) == 2):
+    if (len(y.shape) == 2):
         y = np.array([ y, y, y ])
         y = np.transpose(y, (1, 2, 0))
     y = np.expand_dims(y, axis=0)
@@ -44,18 +44,17 @@ for i, array in enumerate(x_test):
     ## 推論結果で一番可能性が高いものを取得
     y_class = y_probs.argmax(axis=-1)
 
-    ## build result
+    ## 結果を蓄積
     result = {}
     result['image'] = image
     result['class'] = labels[str(y_class[0])]
     result['prob'] = y_probs[0][y_class][0]
     results.append(result)
 
-# sort results
+## ソート
 results = sorted(results, reverse=True, key=lambda v: v['prob'])
 
-## show detail
-## parse results
+## 結果を表示
 results_list = np.reshape(results[:30], (-1, 3))
 
 for j, results in enumerate(results_list):
